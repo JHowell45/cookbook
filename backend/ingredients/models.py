392bb250f3c8409b9a25eabfef324a95a45f4c50
Jacob <jacob@notactivelylooking.com>
2023-03-13 22:45:12 +0000

    Creating root Ingredients model.

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    amount = models.IntegerField()
+    unit = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='images/ingredients/')
+
+    def __str__(self):
+        return self.name
