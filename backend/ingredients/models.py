91172403b0650a94f1bd8a344767ab7965f506e9
Jacob <jacob@notactivelylooking.com>
2023-03-13 23:07:58 +0000

    updating the Ingredient model.

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    amount = models.IntegerField()
-    unit = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='images/ingredients/')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.description}, {self.price}"
