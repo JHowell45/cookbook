76e1835b3c22f20aafa9774ec2df2cc9274ce899
Jacob <jacob@notactivelylooking.com>
2023-03-14 08:53:26 +0000

    Updating main to include axios and commented base URL.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import axios
+// import axios from 'axios'
+
+// set a prototype for http
+// Vue.prototype.$http = axios;
+
+import axios from 'axios'
+
+// axios.defaults.baseURL = 'http://0.0.0.0:8000'
+
 createApp(App).mount('#app')
