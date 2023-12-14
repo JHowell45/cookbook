4565fef484a4d885f148a372b3ff7f3895b15fd0
Jacob Howell <jhowell45@googlemail.com>
2023-12-14 00:44:48 +0000

    point to docker db.

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cookbook_db',
+        'USER': 'example',
+        'PASSWORD': 'example',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
